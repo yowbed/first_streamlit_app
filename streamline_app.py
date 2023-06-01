864980b4a8d7217359fa50e87e1fb0037a8d1146
yowbed <135188763+yowbed@users.noreply.github.com>
2023-06-01 14:10:38 +0800

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,4 +1,1 @@
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
